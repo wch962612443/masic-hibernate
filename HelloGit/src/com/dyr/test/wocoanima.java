b1a71bf67f27e113845842f10c1af2a3b7acc032
wch962612443 <962612443@qq.com>
2015-03-10 17:08:11 +0800

    wocaonimadeaiqing

---
+++ HelloGit/src/com/dyr/test/wocoanima.java
@@ -0,0 +1,28 @@
+/*
+ * www.dyr.com
+ * Copyright (c) 2014 Rights Reserved.
+ */
+
+/**
+ * 
+ */
+package com.dyr.test;
+
+/**
+ * Project:HelloGit
+ * Package:com.dyr.test
+ * FileName:wocoanima.java
+ * Comments:
+ * JDK Version:
+ * Author:KeVin
+ * Create Date:2015-3-10 обнГ5:07:14
+ * Modified By:
+ * Modified Time:
+ * What is Modified:
+ * Version:
+ */
+public class wocoanima {
+	public static void main(String[] args) {
+		System.out.println("wocaonimadeaiqing");
+	}
+}
