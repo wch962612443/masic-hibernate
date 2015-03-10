1405f7099296cb37db623bec7906d74400337497
wch962612443 <962612443@qq.com>
2015-03-10 16:37:59 +0800

    the second HelloGit =>(create HelloGit.java)

---
+++ HelloGit/src/com/dyr/test/HelloGit.java
@@ -0,0 +1,28 @@
+/*
+ * www.dyr.com
+ * Copyright (c) 2014 Rights Reserved.
+ */
+
+/**
+ * 
+ */
+package com.dyr.test;
+
+/**
+ * Project:HelloGit
+ * Package:com.dyr.test
+ * FileName:HelloGit.java
+ * Comments:
+ * JDK Version:
+ * Author:KeVin
+ * Create Date:2015-3-10 обнГ4:35:07
+ * Modified By:
+ * Modified Time:
+ * What is Modified:
+ * Version:
+ */
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!!!");
+	}
+}
